8be0462f8b4ec389376984b4b4afdb0f6448f17d
VladyslavMykhailiuk <109850782+VladyslavMykhailiuk@users.noreply.github.com>
2023-02-09 14:18:31 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -33,26 +33,26 @@
 //echo slug('Hello World');
 
 // Завдання 4
-
-
-//function getResultAboutShopping()
-//{
-//    $products = [
-//        ['name' => 'Phone', 'price' => '500', 'quantity' => 2],
-//        ['name' => 'TV', 'price' => '700', 'quantity' => 1],
-//        ['name' => 'Notebook', 'price' => '600', 'quantity' => 4],
-//    ];
 //
-//    foreach ($products as $product) {
+//$products = [
+//    ['name' => 'Phone', 'price' => '500', 'quantity' => 2],
+//    ['name' => 'TV', 'price' => '700', 'quantity' => 1],
+//    ['name' => 'Notebook', 'price' => '600', 'quantity' => 4],
+//];
+//function getResultAboutShopping(array $arr) :string
+//{
+//
+//
+//    foreach ($arr as $product) {
 //        $sumArr[] = $product['price'] * $product['quantity'];
 //        $sum = array_sum($sumArr);
 //    }
-//    $quantity = array_sum(array_column($products,'quantity'));
+//    $quantity = array_sum(array_column($arr,'quantity'));
 //
 //   return "Сума покупок: $sum, Загальна к-сть товарів: $quantity";
 //}
 //
-//echo getResultAboutShopping();
+//echo getResultAboutShopping($products);
 
 // Завдання 5
 
@@ -84,3 +84,197 @@
 //echo simpleCalc(10,0,'/');
 
 // Завдання 6
+//$array = [25, 17, 136, 12, 79, 15];
+//function sumElemArray(array $arr) :int
+//{
+//
+//    $sum = 0;
+//    foreach ($arr as $item) {
+//        $sum += pow($item, 2);
+//    }
+//    return $sum;
+//}
+//echo sumElemArray($array);
+
+// Завдання 7
+
+//function getOrder(string $str) :string
+//{
+//    $arr = explode(' ', $str);
+//    sort($arr);
+//    $str = implode(' ', $arr);
+//
+//    return $str;
+//}
+//
+//echo getOrder("ноти акустика гітара");
+//echo getOrder("д б г ф а р н");
+
+// Завдання 8
+
+//function truncateString(string $str, int $maxsymbol) :string
+//{
+//    if (mb_strlen($str) > $maxsymbol) {
+//
+//        return mb_substr($str, 0, $maxsymbol+1) . '...';
+//    } else {
+//        return $str;
+//    }
+//}
+//
+//echo truncateString('Some text',5);
+
+// Завдання 9
+
+//function getAbbreviation(string $str) :string
+//{
+//    $string = mb_strtolower($str);
+//
+//    $abbreviation = mb_strtoupper(mb_substr($string, 0, 1));
+//    for ($i = 1; $i < mb_strlen($string); $i++) {
+//        if (mb_substr($string, $i, 1) == ' ') {
+//            $symbol = mb_strtoupper(mb_substr($string, $i + 1, 1));
+//            $abbreviation .= $symbol;
+//        }
+//    }
+//
+//    return $abbreviation;
+//}
+//echo getAbbreviation('Збройні сили України');
+//echo getAbbreviation('Українські телевізійні новини');
+//echo getAbbreviation('Команда електронних бібліотек України');
+
+// Завдання 10
+
+//function getStringWithFirstBigLetter(string $str) :string
+//{
+//    $str = mb_convert_case($str, MB_CASE_TITLE, "UTF-8");
+//    return $str;
+//}
+//
+//echo getStringWithFirstBigLetter('livo pravo veth');
+//echo getStringWithFirstBigLetter('Fivo oslo lviv');
+//echo getStringWithFirstBigLetter('push kick Reth');
+//echo getStringWithFirstBigLetter('рима люкс шик');
+
+// Завдання 11
+
+//function formatEmpty(?string $i='') : ?string
+//{
+//    if(mb_strlen($i) >0) {
+//        return $i;
+//    }
+//else return "Немає даних";
+//}
+//
+//echo formatEmpty();
+
+// Завдання 12
+
+//$users = [
+//    [
+//        'id' => 1,
+//        'name' => 'asdsada1',
+//        // ...
+//    ],
+//    [
+//        'id' => 2,
+//        'name' => 'asdsada2',
+//        // ...
+//    ],
+//];
+//function pluck(array $array,?string $key) : array
+//{
+//    return array_column($array,$key);
+//}
+//
+//print_r(pluck($users,'name'));
+
+// Завдання 13
+
+//$users = [
+//    [
+//        'id' => 1,
+//        'name' => 'asdsada1',
+//        // ...
+//    ],
+//    [
+//        'id' => 2,
+//        'name' => 'asdsada2',
+//        // ...
+//    ],
+//];
+//
+//function findWhere(array $array,?string $key,?string $value) :array|string
+//{
+//foreach ($array as $item) {
+//    if($item[$key]==$value) {
+//        return $item;
+//    }
+//}
+//return 'Упс, такого значення в масиві не знайдено';
+//}
+//
+//print_r(findWhere($users,'id','12'));
+
+// Завдання 14
+//
+//function exampleGenerator(int $totalExamples,string $sign,?int $minNum,?int $maxNum,bool $showAnswer = true,bool $omittingNumber = false) :void
+//{
+//    for($i=1;$i<=$totalExamples;$i++) {
+//        $randomNumber1 = mt_rand($minNum,$maxNum);
+//        $randomNumber2 = mt_rand($minNum,$maxNum);
+//        $signsArr = ['+','-','*','/'];
+//        $newSignsArr = explode(',', $sign);
+//        $randomSign = $newSignsArr[array_rand($newSignsArr)];
+//        $resultOperation = 0;
+//        if(in_array($randomSign,$signsArr)){
+//            switch ($randomSign) {
+//                case '*':
+//                    $resultOperation = $randomNumber1 * $randomNumber2;
+//                    break;
+//                case '+':
+//                    $resultOperation = $randomNumber1 + $randomNumber2;
+//                    break;
+//                case '-':
+//                    $resultOperation = $randomNumber1 - $randomNumber2;
+//                    break;
+//                case '/':
+//                    if($randomNumber2 == 0) {
+//                        echo 'На 0 ділити не можна';
+//                    }
+//                    else {
+//                        $resultOperation= $randomNumber1 / $randomNumber2;
+//                    }
+//                    break;
+//            }
+//        }
+//        else {
+//            echo 'Ви ввели некоректний знак';
+//        }
+//        if($showAnswer && $omittingNumber) {
+//            $res = "$randomNumber1 $randomSign $randomNumber2 = $resultOperation";
+//                $arrayOfRes =  explode(' ',$res);
+//                $arrayOfRes[array_rand($arrayOfRes)] = '_';
+//                $newString = implode(' ',$arrayOfRes);
+//                echo "$newString <br>";
+//        }
+//        elseif ($showAnswer && !$omittingNumber) {
+//            echo "$randomNumber1 $randomSign $randomNumber2 = $resultOperation <br>";
+//        }
+//        elseif (!$showAnswer && $omittingNumber) {
+//            $res = "$randomNumber1 $randomSign $randomNumber2";
+//                $arrayOfRes =  explode(' ',$res);
+//                $arrayOfRes[array_rand($arrayOfRes)] = '_';
+//                $newString = implode(' ',$arrayOfRes);
+//                echo "$newString = ... <br>";
+//        }
+//        elseif (!$showAnswer && !$omittingNumber) {
+//            echo "$randomNumber1 $randomSign $randomNumber2 = ... <br>";
+//        }
+//
+//    }
+//}
+//
+//echo exampleGenerator(10,'+,-,!',1,10,true,false);
+
